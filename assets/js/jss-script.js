fd185f7468f1074a7670bff1dea7684632bce488
scaibe <luca.grafica@gmail.com>
2021-01-19 17:45:07 +0100

    Test con tiny slider

---
+++ assets/js/jss-script.js
@@ -0,0 +1,12 @@
+(function($){
+
+  $(document).ready(function(){
+    var slider = tns({
+      container: '.homepage-slider',
+      items: 1,
+      slideBy: 'page',
+      autoplay: true
+    });
+  });
+  
+})(jQuery);
